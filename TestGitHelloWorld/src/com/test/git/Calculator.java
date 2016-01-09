7df4cc0373c3a96f6a49288d2ea3da467fa41895
Prachi <Prachi@192.168.1.4>
2016-01-09 19:13:06 +0530

    Calling Calculator.add from HelloWorld

--- TestGitHelloWorld/src/com/test/git/Calculator.java
+++ TestGitHelloWorld/src/com/test/git/Calculator.java
@@ -3,6 +3,7 @@ package com.test.git;
 public class Calculator {
 	
 	public static int add(int num1, int num2){
+		//adding num1 and num2
 		return num1+num2;
 	}
 
--- TestGitHelloWorld/src/com/test/git/HelloWorld.java
+++ TestGitHelloWorld/src/com/test/git/HelloWorld.java
@@ -3,8 +3,10 @@ package com.test.git;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		//calling calculator
+		int value = Calculator.add(4, 9);
 		System.out.println("Hello World to Git");
-
+		System.out.println("Value "+value);
 	}
 
 }
